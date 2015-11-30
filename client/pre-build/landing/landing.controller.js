295749816bab3168cf01d7fa3b78facc15dfd92b
dwschrashun <dwschrashun@gmail.com>
2015-11-30 00:28:01 -0500

    spinner

--- client/pre-build/landing/landing.controller.js
+++ client/pre-build/landing/landing.controller.js
@@ -1,7 +1,9 @@
 app.controller('LandingController', function($scope, CountryFactory, $state) {
+	$scope.loading = false;
 	$scope.countries = CountryFactory.countries;
 	$scope.country = "--Select Country--";
 	$scope.goToHome = function (city, country) {
+		$scope.loading = true;
 		$state.go("home", {city: city, country: country});
 	};
 });
