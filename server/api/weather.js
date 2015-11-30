5dabcd607c158fa25563aaa488cd098ec5c0f348
dwschrashun <dwschrashun@gmail.com>
2015-11-30 00:08:24 -0500

    environment variable change

--- server/api/weather.js
+++ server/api/weather.js
@@ -1,7 +1,14 @@
 var router = require('express').Router();
-var key = require("../../secrets.json").openWMKey;
+var key;
 var http = require("http");
 module.exports = router;
+
+if (process.env.NODE_ENV === 'production') {
+	key = require("../../secrets.json").openWMKey;
+}
+else {
+	key = process.env.OWMKey;
+}
 
 router.get("/", function (req, res, next) {
 	console.log("get", req.query);
@@ -9,7 +16,6 @@ router.get("/", function (req, res, next) {
 		if(!req.query.countryCode) {
 			req.query.countryCode = "";
 		}
-		var requestKey = key || process.env.OWMKey;
 		var queryString = `http://api.openweathermap.org/data/2.5/forecast/daily?q=${req.query.city},${req.query.countryCode}&cnt=16&type=like&units=imperial&APPID=${key}`;
 		http.get(queryString, function (response) {
 			var body = "";
